594fc4456d706cb628670b3b37b3198d0cea2c92
Richard van der Dys <richard.vanderdys@gmail.com>
2020-07-27 16:49:55 +0300

    Clean up

--- src/session.js
+++ src/session.js
@@ -1,6 +1,7 @@
 import getClient, { encodeUser } from './utils/client'
 import { COOKIES } from './utils/constants'
 import normalizeProduct from './utils/normalizeProduct'
+import { product } from '.'
 
 export default async function session(req, res) {
   const client = await getClient(req)
@@ -9,12 +10,16 @@ export default async function session(req, res) {
 
   const { productItems, productTotal } = await client.getCart()
 
-  const products = await client.getProducts({
-    ids: productItems.map(item => item.productId).join(','),
-  })
-  const items = (products.data || []).map((item, index) => {
-    return normalizeProduct({ ...productItems[index], ...item })
-  })
+  let items = []
+
+  if (productItems && productItems.length) {
+    const products = await client.getProducts({
+      ids: productItems.map(item => item.productId).join(','),
+    })
+    items = (products.data || []).map((item, index) => {
+      return normalizeProduct({ ...productItems[index], ...item })
+    })
+  }
 
   res.setHeader('Set-Cookie', `${COOKIES.USER}=${encodeUser(client.user)}; Path=/`)
 
